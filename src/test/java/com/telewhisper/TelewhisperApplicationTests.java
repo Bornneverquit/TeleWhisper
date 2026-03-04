6d934d572350557d18527742c8c9b1aa7417a9e0
Bornneverquit <praveenakm0@gmail.com>
2026-03-04 22:14:43 +0530

    Initial Spring Project Structure

---
+++ src/test/java/com/telewhisper/TelewhisperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telewhisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelewhisperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
